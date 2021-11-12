f9f2ae841d9922869e2ad16047fc9aa96443149f
István Szorcsik <szorcsik.istvan12@gmail.com>
2021-11-12 19:50:26 +0100

    Color Balls

--- Software Design Patterns/Form1.cs
+++ Software Design Patterns/Form1.cs
@@ -68,7 +68,10 @@ namespace Software_Design_Patterns
 
         private void ballButton_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory()
+            {
+                BallColor = colorButton.BackColor
+            };
         }
 
         private void DisplayNext()
